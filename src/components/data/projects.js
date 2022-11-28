930c1d612157fcafe433614013ed9d61dcf38ebe
Tushar Pathak <tusharpathak2503@gmail.com>
2022-11-28 10:03:55 +0530

    added a new project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,22 +1,14 @@
 export const ProjectData = [
     {
         id:1,
-        title:"Landing Page of a Gym",
-        about:"This is a simple landing page of a gym",
-        tags:["Html", "Css"],
-        github: 'https://github.com/tushar54594/Landing-Page-of-a-Gym',
-        image:'https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png'
+        title:"Travel Website",
+        about:"Landing page of a Travel agency",
+        tags:["React", "Tailwindcss"],
+        github: 'https://github.com/tushar54594/TravelWebsite',
+        image:'https://user-images.githubusercontent.com/94536522/204193824-8f6911b0-ca9a-4fe4-8e84-d19e80780a59.png'
     },
     {
         id:2,
-        title:"Landing page of a BMW Car",
-        about:"This is a website design for a car where we can click on the button to see the different colours of the cars",
-        tags:["Html", "Css", "JavaScript"],
-        github: 'https://github.com/tushar54594/Website-design-for-car-where-we-can-click-on-button-to-see-the-different-variants-of-car',
-        image:'https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png'
-    },
-    {
-        id:3,
         title:"Tesla Clone",
         about:"This is a clone of the Tesla website",
         tags:["Html", "Css", "JavaScript"],
@@ -24,7 +16,24 @@ export const ProjectData = [
         image:'https://user-images.githubusercontent.com/94536522/178146616-dece2cb3-83e3-44ec-bb00-87daf5502cc2.png'
     },
     {
+        id:3,
+        title:"Landing Page of a Gym",
+        about:"This is a simple landing page of a gym",
+        tags:["Html", "Css"],
+        github: 'https://github.com/tushar54594/Landing-Page-of-a-Gym',
+        image:'https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png'
+    },
+    {
         id:4,
+        title:"Landing page of a BMW Car",
+        about:"This is a website design for a car where we can click on the button to see the different colours of the cars",
+        tags:["Html", "Css", "JavaScript"],
+        github: 'https://github.com/tushar54594/Website-design-for-car-where-we-can-click-on-button-to-see-the-different-variants-of-car',
+        image:'https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png'
+    },
+    
+    {
+        id:5,
         title:"Fun Facts about React",
         about:"This is a page giving information about the fun facts of react",
         tags:["Html", "Css", "React"],

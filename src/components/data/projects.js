1a70db4bf773792896b28a29ed8b82b6adb18fe9
Tushar Pathak <tusharpathak2503@gmail.com>
2023-03-04 22:33:34 +0530

    new project added

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,52 +1,68 @@
 export const ProjectData = [
-    {
-        id:1,
-        title:"Todo App",
-        about:"This is a Todo Application. Here you can add new tasks, mark the tasks as completed and also delete the tasks.",
-        tags:["React", "Tailwindcss"],
-        github: 'https://github.com/tushar54594/TodoApp',
-        image:'https://user-images.githubusercontent.com/94536522/205562051-5d75b7b2-dc0d-454d-9069-c9581afa55ed.png'
-    },
-    {
-        id:1,
-        title:"Travel Website",
-        about:"Landing page of a Travel agency",
-        tags:["React", "Tailwindcss"],
-        github: 'https://github.com/tushar54594/TravelWebsite',
-        image:'https://user-images.githubusercontent.com/94536522/204193824-8f6911b0-ca9a-4fe4-8e84-d19e80780a59.png'
-    },
-    {
-        id:2,
-        title:"Tesla Clone",
-        about:"This is a clone of the Tesla website",
-        tags:["Html", "Css", "JavaScript"],
-        github: 'https://github.com/tushar54594/Tesla-Clone',
-        image:'https://user-images.githubusercontent.com/94536522/178146616-dece2cb3-83e3-44ec-bb00-87daf5502cc2.png'
-    },
-    {
-        id:3,
-        title:"Landing Page of a Gym",
-        about:"This is a simple landing page of a gym",
-        tags:["Html", "Css"],
-        github: 'https://github.com/tushar54594/Landing-Page-of-a-Gym',
-        image:'https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png'
-    },
-    {
-        id:4,
-        title:"Landing page of a BMW Car",
-        about:"This is a website design for a car where we can click on the button to see the different colours of the cars",
-        tags:["Html", "Css", "JavaScript"],
-        github: 'https://github.com/tushar54594/Website-design-for-car-where-we-can-click-on-button-to-see-the-different-variants-of-car',
-        image:'https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png'
-    },
-    
-    {
-        id:5,
-        title:"Fun Facts about React",
-        about:"This is a page giving information about the fun facts of react",
-        tags:["Html", "Css", "React"],
-        github: 'https://github.com/tushar54594/FunFactsAboutReact',
-        image:'https://user-images.githubusercontent.com/94536522/202886234-fdfeddcb-d1a7-43ca-9a63-15caabfb4ba8.png'
-    },
-
-]
+  {
+    id: 1,
+    title: "Todo App",
+    about:
+      "This is a Todo Application. Here you can add new tasks, mark the tasks as completed and also delete the tasks.",
+    tags: ["React", "Tailwindcss"],
+    github: "https://github.com/tushar54594/TodoApp",
+    image:
+      "https://user-images.githubusercontent.com/94536522/205562051-5d75b7b2-dc0d-454d-9069-c9581afa55ed.png",
+  },
+  {
+    id: 2,
+    title: "Travel Website",
+    about: "Landing page of a Travel agency",
+    tags: ["React", "Tailwindcss"],
+    github: "https://github.com/tushar54594/TravelWebsite",
+    image:
+      "https://user-images.githubusercontent.com/94536522/204193824-8f6911b0-ca9a-4fe4-8e84-d19e80780a59.png",
+  },
+  {
+    id: 3,
+    title: "TicTacToe Game",
+    about: "This is a Tic Tac Toe game",
+    tags: ["React", "CSS"],
+    github: "https://github.com/tushar54594/TicTacToeGame",
+    image:
+      "https://user-images.githubusercontent.com/94536522/222918949-da8d3e3e-dae2-4f37-a8c7-3b0858da2aa6.png",
+  },
+  {
+    id: 4,
+    title: "Tesla Clone",
+    about: "This is a clone of the Tesla website",
+    tags: ["Html", "Css", "JavaScript"],
+    github: "https://github.com/tushar54594/Tesla-Clone",
+    image:
+      "https://user-images.githubusercontent.com/94536522/178146616-dece2cb3-83e3-44ec-bb00-87daf5502cc2.png",
+  },
+  {
+    id: 5,
+    title: "Landing Page of a Gym",
+    about: "This is a simple landing page of a gym",
+    tags: ["Html", "Css"],
+    github: "https://github.com/tushar54594/Landing-Page-of-a-Gym",
+    image:
+      "https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png",
+  },
+  {
+    id: 6,
+    title: "Landing page of a BMW Car",
+    about:
+      "This is a website design for a car where we can click on the button to see the different colours of the cars",
+    tags: ["Html", "Css", "JavaScript"],
+    github:
+      "https://github.com/tushar54594/Website-design-for-car-where-we-can-click-on-button-to-see-the-different-variants-of-car",
+    image:
+      "https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png",
+  },
+  {
+    id: 7,
+    title: "Fun Facts about React",
+    about: "This is a page giving information about the fun facts of react",
+    tags: ["Html", "Css", "React"],
+    github: "https://github.com/tushar54594/FunFactsAboutReact",
+    image:
+      "https://user-images.githubusercontent.com/94536522/202886234-fdfeddcb-d1a7-43ca-9a63-15caabfb4ba8.png",
+  },
+];

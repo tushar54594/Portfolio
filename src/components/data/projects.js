6f1dfdb282282153e962822b50c6fd8791936584
Tushar Pathak <tusharpathak2503@gmail.com>
2023-03-09 16:52:08 +0530

    new project added

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,6 +1,16 @@
 export const ProjectData = [
   {
     id: 1,
+    title: " Onicron - Interface to connect Entrepreneurs and VCs",
+    about:
+      "A platform to connect Entrepreneurs and VCs. Entrepreneurs can pitch their ideas and VCs can look at all ideas present and chat with a particular entrepreneur if they like their idea.",
+    tags: ["React", "Firebase"],
+    github: "https://github.com/tushar54594/Onicron_Interface-to-connect-Entrepreneurs-and-VCs",
+    image:
+      "https://user-images.githubusercontent.com/94536522/224007752-741a98a0-3bf2-4246-a1a5-586e633fe911.png",
+  },
+  {
+    id: 2,
     title: "Todo App",
     about:
       "This is a Todo Application. Here you can add new tasks, mark the tasks as completed and also delete the tasks.",
@@ -10,7 +20,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/205562051-5d75b7b2-dc0d-454d-9069-c9581afa55ed.png",
   },
   {
-    id: 2,
+    id: 3,
     title: "Travel Website",
     about: "Landing page of a Travel agency",
     tags: ["React", "Tailwindcss"],
@@ -19,7 +29,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/204193824-8f6911b0-ca9a-4fe4-8e84-d19e80780a59.png",
   },
   {
-    id: 3,
+    id: 4,
     title: "TicTacToe Game",
     about: "This is a Tic Tac Toe game",
     tags: ["React", "CSS"],
@@ -28,7 +38,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/222918949-da8d3e3e-dae2-4f37-a8c7-3b0858da2aa6.png",
   },
   {
-    id: 4,
+    id: 5,
     title: "Tesla Clone",
     about: "This is a clone of the Tesla website",
     tags: ["Html", "Css", "JavaScript"],
@@ -37,7 +47,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/178146616-dece2cb3-83e3-44ec-bb00-87daf5502cc2.png",
   },
   {
-    id: 5,
+    id: 6,
     title: "Landing Page of a Gym",
     about: "This is a simple landing page of a gym",
     tags: ["Html", "Css"],
@@ -46,7 +56,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png",
   },
   {
-    id: 6,
+    id: 7,
     title: "Landing page of a BMW Car",
     about:
       "This is a website design for a car where we can click on the button to see the different colours of the cars",
@@ -57,7 +67,7 @@ export const ProjectData = [
       "https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png",
   },
   {
-    id: 7,
+    id: 8,
     title: "Fun Facts about React",
     about: "This is a page giving information about the fun facts of react",
     tags: ["Html", "Css", "React"],

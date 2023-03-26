e11e7cb2b29712d458a0301b686fb8bcd557d0f2
Tushar Pathak <tusharpathak2503@gmail.com>
2023-03-26 21:36:21 +0530

    old projects removed

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,16 +5,16 @@ export const ProjectData = [
     about:
       "A platform to connect Entrepreneurs and VCs. Entrepreneurs can pitch their ideas and VCs can look at all ideas present and chat with a particular entrepreneur if they like their idea.",
     tags: ["React", "Firebase"],
-    github: "https://github.com/tushar54594/Onicron_Interface-to-connect-Entrepreneurs-and-VCs",
+    github:
+      "https://github.com/tushar54594/Onicron_Interface-to-connect-Entrepreneurs-and-VCs",
     image:
       "https://user-images.githubusercontent.com/94536522/224007752-741a98a0-3bf2-4246-a1a5-586e633fe911.png",
   },
   {
     id: 2,
     title: "Netflix Clone",
-    about:
-      "This is a Netflix clone created using react and firebase.",
-    tags: ["React","Tailwindcss", "Firebase"],
+    about: "This is a Netflix clone created using react and firebase.",
+    tags: ["React", "Tailwindcss", "Firebase"],
     github: "https://github.com/tushar54594/Netflix-Clone",
     image:
       "https://user-images.githubusercontent.com/94536522/224562500-f5bd66a9-0c25-469f-87e4-fc11af8a3baf.png",
@@ -46,43 +46,5 @@ export const ProjectData = [
     github: "https://github.com/tushar54594/TicTacToeGame",
     image:
       "https://user-images.githubusercontent.com/94536522/222918949-da8d3e3e-dae2-4f37-a8c7-3b0858da2aa6.png",
-  },
-  {
-    id: 6,
-    title: "Tesla Clone",
-    about: "This is a clone of the Tesla website",
-    tags: ["Html", "Css", "JavaScript"],
-    github: "https://github.com/tushar54594/Tesla-Clone",
-    image:
-      "https://user-images.githubusercontent.com/94536522/178146616-dece2cb3-83e3-44ec-bb00-87daf5502cc2.png",
-  },
-  {
-    id: 7,
-    title: "Landing Page of a Gym",
-    about: "This is a simple landing page of a gym",
-    tags: ["Html", "Css"],
-    github: "https://github.com/tushar54594/Landing-Page-of-a-Gym",
-    image:
-      "https://user-images.githubusercontent.com/94536522/178135479-d1c1885a-33c4-4d1e-89de-de9755f5309d.png",
-  },
-  {
-    id: 8,
-    title: "Landing page of a BMW Car",
-    about:
-      "This is a website design for a car where we can click on the button to see the different colours of the cars",
-    tags: ["Html", "Css", "JavaScript"],
-    github:
-      "https://github.com/tushar54594/Website-design-for-car-where-we-can-click-on-button-to-see-the-different-variants-of-car",
-    image:
-      "https://user-images.githubusercontent.com/94536522/178145898-fe20bb51-93f7-4ba5-8969-a66afb8ccf72.png",
-  },
-  {
-    id: 9,
-    title: "Fun Facts about React",
-    about: "This is a page giving information about the fun facts of react",
-    tags: ["Html", "Css", "React"],
-    github: "https://github.com/tushar54594/FunFactsAboutReact",
-    image:
-      "https://user-images.githubusercontent.com/94536522/202886234-fdfeddcb-d1a7-43ca-9a63-15caabfb4ba8.png",
   },
 ];
